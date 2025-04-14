26e47308ca31b258455818a38ff3dcdff8d10e79
6Morpheus6 <wanton-decay@gmx.net>
2025-04-14 14:03:12 +0200

    remove mps fallback, directml support, launch in pinokio

--- start.js
+++ start.js
@@ -1,52 +1,28 @@
-const os = require('os')
-module.exports = async (kernel) => {
-  let env = {
-    "HF_HOME": "./huggingface",
-  }
-  if (os.platform() === 'darwin') {
-    env.PYTORCH_ENABLE_MPS_FALLBACK = "1",
-    env.PYTORCH_MPS_HIGH_WATERMARK_RATIO = "0.0"
-  }
-  return {
-    "run": [{
-      "method": "shell.start",
-      "params": {
-        "path": "app",
-        "env": env
-      }
-    }, {
-      "method": "shell.enter",
-      "params": {
-        "message": "{{os.platform() === 'win32' ? 'env\\\\Scripts\\\\activate' : 'source env/bin/activate'}} env",
-        "on": [{
-          "event": null,
-          "return": true
+module.exports = {
+  daemon: true,
+  run: [
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",
+        env: {
+          "PYTORCH_MPS_HIGH_WATERMARK_RATIO": "0.0"
+        },
+        path: "app",
+        message: [
+          "{{platform === 'win32' && gpu === 'amd' ? 'python infer-web.py --dml --noautoopen' : 'python infer-web.py --noautoopen'}}",
+        ],
+        on: [{
+          "event": "/http:\/\/\\S+/",
+          "done": true
         }]
       }
-    }, {
-      "method": "shell.enter",
-      "params": {
-        "message": "{{os.platform() === 'win32' ? 'python' : 'python3'}} infer-web.py",
-        "on": [{
-          "event": "/(http:\/\/[0-9.:]+)/",
-          "return": "{{event.matches[0][1]}}"
-        }]
+    },
+    {
+      method: "local.set",
+      params: {
+        url: "http://127.0.0.1:{{input.event[0].split(':').pop()}}"
       }
-    }, {
-      "method": "self.set",
-      "params": {
-        "session.json": {
-          "url": "{{input}}"
-        }
-      }
-    }, {
-      "method": "browser.open",
-      "params": {
-        "uri": "{{self.session.url}}",
-        "target": "_blank"
-      }
-    }, {
-      "method": "process.wait" 
-    }]
-  }
+    }
+  ]
 }
